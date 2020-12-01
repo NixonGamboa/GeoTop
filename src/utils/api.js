847cda2705dcd2cbca3c10d49dc1fc544fc37494
Nixon Gamboa <nrgamboa97@gmail.com>
2020-12-01 14:47:10 -0500

    Haciendo lectura de base de datos con fetch

---
+++ src/utils/api.js
@@ -0,0 +1,24 @@
+//consulta a base de datos
+
+class Api {
+	
+   getTopTracks = async () => {
+    try {
+        
+      //define el api key y la url
+      const ApiKey ='829751643419a7128b7ada50de590067';
+      const API_REQUEST =`http://ws.audioscrobbler.com/2.0/?method=geo.gettoptracks&country=colombia&api_key=${ApiKey}&limit=10&format=json`;
+        
+      //realiza la solicitud y retorna los datos
+      const query = await fetch(API_REQUEST);
+      const data = await query.json();
+      console.log(data.tracks)
+      return data.tracks;
+
+    } catch (error) {
+      console.error(error);
+    }
+  };
+}
+
+export default new Api();
